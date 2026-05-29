db51eba0046b0d70e6822fafc143ebcdd9abd317
andika9199 <sociolla@la-dita-tech-lw.bellanina.com>
2026-05-29 17:31:34 +0700

    revise minus image

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -255,10 +255,10 @@ export const guest = (() => {
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = new URLSearchParams({
             action: 'TEMPLATE',
-            text: 'The Wedding of Wahyu and Riski',
-            dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
+            text: 'The Wedding of Dika and Joely',
+            dates: `${formatDate('2026-07-05 10:00')}/${formatDate('2026-07-05 12:00')}`,
             details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
-            location: 'RT 10 RW 02, Desa Pajerukan, Kec. Kalibagor, Kab. Banyumas, Jawa Tengah 53191.',
+            location: 'RT',
             ctz: config.get('tz'),
         });
 
